dc510b0ce4e7cd107bdf7e8f96b6b56f8ac4025c
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-17 17:34:36 -0300

    basic react elements

---
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EmptyCart from './EmptyCart';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return <EmptyCart />;
+  }
+}
+
+export default ShoppingCart;
