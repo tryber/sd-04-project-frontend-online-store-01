c0a089529faaf654c24faf60c28157c3ad1273da
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-17 18:14:38 -0300

    Adicionado botão para finalizar compra

--- src/pages/ShoppingCart/CartTotals.js
+++ src/pages/ShoppingCart/CartTotals.js
@@ -1,1 +1,12 @@
-// teste2
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CartTotals() {
+  return (
+    <Link to="/checkout">
+      <button type="button">Click Me!</button>
+    </Link>
+  );
+}
+
+export default CartTotals;
