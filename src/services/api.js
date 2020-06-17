ca1902575c7535eb33373bf3839f7f7c69d8e795
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-17 18:08:31 -0300

    requisitos 1 e 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export async function getCategories() {
-  const request = fetch("https://api.mercadolibre.com/sites/MLB/categories")
+  const request = fetch('https://api.mercadolibre.com/sites/MLB/categories')
   .then((response) => response.json());
   return request;
 }
