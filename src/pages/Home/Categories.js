f4084b233f9e8db312fde5353b1d175001207db7
deboracosilveira <deboracosilveira@gmail.com>
2020-06-17 15:37:48 -0300

    initial commit

---
+++ src/pages/Home/Categories.js
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react';
+
+// class Categories extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return <div></div>;
+//   }
+// }
+
+// export default Categories;
+// To commit
