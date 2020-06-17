178b82dfece4550f64d4bccfca8f870d271ceca3
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-17 18:06:15 -0300

    requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as api from './services/api';
 import ProductList from './pages/Home/ProductList';
 
 function App() {
-  api.getCategories().then(categories => { console.log(categories) });
+  api.getCategories().then((categories) => { console.log(categories); });
   return (
     <div className="App">
-    <ProductList />
+      <ProductList />
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export async function getCategories() {
   const request = fetch("https://api.mercadolibre.com/sites/MLB/categories")
-  .then(response => response.json())
+  .then((response) => response.json());
   return request;
 }
 
