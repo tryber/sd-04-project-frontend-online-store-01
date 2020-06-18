3517e0715fbbaf2eb62edb92891fc9d7fd37efb5
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-18 11:39:19 -0300

    testando font-awesome com o CC

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
 class ProductDetails extends React.Component {
   render() {
     return (
-      <Link data-testid="shopping-cart-button" to="/shoppingcart">
-        Carrinho de compras
-      </Link>
+      <>
+        <div>
+          <Link data-testid="shopping-cart-button" to="/shoppingcart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            Carrinho de compras
+          </Link>
+        </div>
+        <div className="product-details-wrapper">
+          <h2 data-testid="product-datail-name">Nome do produto</h2>
+        </div>
+      </>
     );
   }
 }
