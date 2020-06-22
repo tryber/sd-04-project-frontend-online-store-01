88c684a8886d39f5e3e9e269adecdc6e1c2e17e0
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-22 12:43:11 -0300

    req 11 and 9

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -5,12 +5,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 import { getProductsFromCategoryAndQuery } from '../../services/api';
+import Review from './Review';
 
 class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
-    const { location: { state: { product } } } = props;
-    this.state = { product };
+    const product = props.location.state ? props.location.state.product : null;
+    this.state = { product, reviewInput: '' };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount() {
@@ -23,24 +26,41 @@ class ProductDetails extends React.Component {
   }
 
   handleProduct(data) {
-    this.setState({ product: { data } });
+    this.setState({ product: { ...data } });
   }
 
   cartQuantityRender() {
     const { cartProducts } = this.props;
-    const { product: { id } } = this.state;
+    const {
+      product: { id },
+    } = this.state;
     const cartProductCheck = () => cartProducts.find((p) => p.id === id);
     const cartQuantity = cartProductCheck()
       ? cartProductCheck().cartQuantity
       : 0;
     return <p>Quantidade no Carrinho: {cartQuantity}</p>;
   }
+  // form handlers
+
+  handleInputChange(e) {
+    this.setState({ reviewInput: e.target.value });
+  }
+
+  handleSubmit(e) {
+    console.log('teste');
+    e.preventDefault();
+    this.setState({ reviewInput: '' });
+  }
 
   render() {
+    if (!this.state.product) {
+      return <div>Carregando...</div>;
+    }
     const { addToCart, subFromCart, removeFromCart } = this.props;
     const {
       product,
       product: { thumbnail, title, price },
+      reviewInput,
     } = this.state;
     return (
       <div>
@@ -55,7 +75,11 @@ class ProductDetails extends React.Component {
           <img src={thumbnail} alt="Imagem do produto" />
           {this.cartQuantityRender()}
           <p> Preço: R${price} </p>
-          <button type="button" onClick={() => addToCart(product)}>
+          <button
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            onClick={() => addToCart(product)}
+          >
             Adicionar ao Carrinho
           </button>
           <button type="button" onClick={() => subFromCart(product)}>
@@ -65,6 +89,11 @@ class ProductDetails extends React.Component {
             Remover do Carrinho
           </button>
         </div>
+        <Review
+          handleSubmit={this.handleSubmit}
+          reviewInput={reviewInput}
+          handleInputChange={this.handleInputChange}
+        />
       </div>
     );
   }
