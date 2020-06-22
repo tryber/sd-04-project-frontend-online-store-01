ce4f92709493d0ab15dc930137a2201164e26295
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-22 20:18:53 -0300

    bug fix

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 import { getProductsFromCategoryAndQuery } from '../../services/api';
 import Review from './Review';
